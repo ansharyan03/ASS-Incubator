89c5d36c719d9ffe5db2735040c280ca68a3c7e7
Sushant Marella <sushantmarella@sushants-air.wireless-1x.unc.edu>
2023-11-18 11:57:20 -0500

    Textbox

--- webserver/src/App.tsx
+++ webserver/src/App.tsx
@@ -2,52 +2,63 @@ import React, { useState } from 'react';
 
 const LibraryOccupancyTracker: React.FC = () => {
   const [selectedFloor, setSelectedFloor] = useState<number | null>(null);
-
-  // Create an object to store the occupancy information for each floor
-  const [occupancyData, setOccupancyData] = useState<{ [key: number]: number }>({
-    1: 0, // Initialize occupancy for each floor to 0
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-  });
+  const [occupancyData, setOccupancyData] = useState<number[]>([0, 0, 0, 0, 0, 0, 0, 0]);
+  const [username, setUsername] = useState<string>('');
+  const [note, setNote] = useState<string>('');
 
   const handleFloorClick = (floor: number) => {
     if (selectedFloor === floor) {
-      // If the same floor is clicked again, unselect it and decrease its occupancy
       setSelectedFloor(null);
-      setOccupancyData({
-        ...occupancyData,
-        [floor]: occupancyData[floor] - 1,
+      setOccupancyData(prevOccupancyData => {
+        const newData = [...prevOccupancyData];
+        newData[floor - 1] = Math.max(newData[floor - 1] - 1, 0);
+        return newData;
       });
     } else {
-      // Otherwise, update the occupancy for the previously selected floor and set the new floor
       setSelectedFloor(floor);
-      setOccupancyData({
-        ...occupancyData,
-        [selectedFloor!]: occupancyData[selectedFloor!] - 1, // Decrease occupancy for the previously selected floor
-        [floor]: occupancyData[floor] + 1, // Increase occupancy for the new floor
+      setOccupancyData(prevOccupancyData => {
+        const newData = [...prevOccupancyData];
+        newData[floor - 1] += 1;
+        if (selectedFloor !== null) {
+          newData[selectedFloor - 1] = Math.max(newData[selectedFloor - 1] - 1, 0);
+        }
+        return newData;
       });
     }
   };
 
   return (
-    <div>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
       <h1>Welcome, Please Select What Floor of Davis You'll Be At</h1>
       <p>Selected Floor: {selectedFloor === null ? 'None' : `${selectedFloor} floor`}</p>
 
-      <div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
         {[1, 2, 3, 4, 5, 6, 7, 8].map((floor) => (
-          <div key={floor}>
-            <button onClick={() => handleFloorClick(floor)} className={selectedFloor === floor ? 'selected' : ''}>
+          <div key={floor} style={{ margin: '10px' }}>
+            <button
+              onClick={() => handleFloorClick(floor)}
+              className={`floor-button ${selectedFloor === floor ? 'selected' : ''}`}
+            >
               {floor} Floor
             </button>
-            <span>Occupancy Percentage: {(occupancyData[floor] / 50 * 100).toFixed(2)}%</span>
+            <span>Occupancy: {occupancyData[floor - 1]}</span>
           </div>
         ))}
+      </div>
+
+      <div style={{ marginTop: '20px' }}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+      </div>
+
+      <div style={{ marginTop: '10px' }}>
+        <label>
+          Note:
+          <textarea value={note} onChange={(e) => setNote(e.target.value)} />
+          <h1>{note}</h1>
+        </label>
       </div>
     </div>
   );
